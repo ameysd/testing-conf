a2172104dd65a46efa25c816ef8a8a036a446031
ameysd <2009asdeshpande@gmail.com>
2019-07-05 17:03:28 +0530

    written a python test

---
+++ app/calculator.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+
+class CalcMachine(object):
+	def div(self,a,b):
+       # if isinstance(a,int) and isinstance(b,int):
+            return a/b
+        #else:
+            raise Exception('Invalid arguments')
